5c05ff2e0a85a72cdd194201272f8ff55e518fa3
Abhinav Datta <74416206+abhinavdatta@users.noreply.github.com>
2021-08-17 19:23:17 +0530

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -73,8 +73,9 @@ export default class HomeScreen extends Component{
             style: { color: '#fff', fontSize: 20 },
           }}
         />
-         <h4> click on the link for "a" words other words anre not written here but you can search other words too <a href = "https://drive.google.com/file/d/1X0mDLBhNzIcRmlAvlaf6yyXCg6OOi3TO/view?usp=sharing">click here </a></h4>
+         <h4> click on the link for "a" words other words are not written here but you can search other words too <a href = "https://drive.google.com/file/d/1X0mDLBhNzIcRmlAvlaf6yyXCg6OOi3TO/view?usp=sharing">click here </a></h4>
          
+           <h6>other files in the open source of dictionary is hidden</h6>
          
         <View style={styles.inputBoxContainer}>{
          
